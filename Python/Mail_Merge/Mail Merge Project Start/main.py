3ae29caa86f3ac93473aac4df52e574802f188db
Mansoor Basha Syed <83864755+mansoorsd@users.noreply.github.com>
2021-05-29 11:41:05 +0530

    Removing bugs

--- Python/Mail_Merge/Mail Merge Project Start/main.py
+++ Python/Mail_Merge/Mail Merge Project Start/main.py
@@ -34,18 +34,3 @@ for name in name_list:
     lettr1 = open(f".\Output\ReadyToSend\letter_to_{name}.txt",mode="w")
     lettr1.write(letter)
     lettr1.close()
-
-
-
-
-
-# for word in lettr.readlines():
-
-
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
